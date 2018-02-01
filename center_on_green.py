9f26988bcf3e55ab6157b57862591dcffdbc0d26
Benjamin Alt <benjamin_alt@outlook.com>
2018-02-01 21:10:02 +0100

    Best solution for tracking red cylinders yet: Position control without oscillation using direct feedback from published joint states

--- center_on_green.py
+++ center_on_green.py
@@ -15,13 +15,12 @@ import numpy as np
 @nrp.MapRobotPublisher('eye_pan_pos', Topic('/robot/left_eye_pan/pos', std_msgs.msg.Float64))
 @nrp.MapRobotPublisher('eye_tilt_vel', Topic('/robot/eye_tilt/vel', std_msgs.msg.Float64))
 @nrp.MapRobotPublisher('eye_pan_vel', Topic('/robot/left_eye_pan/vel', std_msgs.msg.Float64))
-@nrp.MapRobotSubscriber("eye_tilt_sub", Topic("/robot/eye_tilt/pos", std_msgs.msg.Float64))
-@nrp.MapRobotSubscriber("eye_pan_sub", Topic("/robot/left_eye_pan/pos", std_msgs.msg.Float64))
+@nrp.MapRobotSubscriber("joint_state_sub", Topic("/robot/joints", sensor_msgs.msg.JointState))
 @nrp.MapRobotSubscriber("shuffle_status_sub", Topic("/group_3/shuffling", std_msgs.msg.Bool))
 @nrp.Neuron2Robot()
 def center_on_green(t, motors_down_stage_one, motors_left_stage_one, motors_up_stage_one, motors_right_stage_one, 
                     motors_down_stage_two, motors_left_stage_two, motors_up_stage_two, motors_right_stage_two, motors_center_stage_two,
-                    eye_tilt_pos, eye_pan_pos, eye_tilt_vel, eye_pan_vel, eye_tilt_sub, eye_pan_sub, shuffle_status_sub):
+                    eye_tilt_pos, eye_pan_pos, eye_tilt_vel, eye_pan_vel, joint_state_sub, shuffle_status_sub):
 
     stage_two = shuffle_status_sub.value.data if shuffle_status_sub.value is not None else False
 
@@ -33,14 +32,17 @@ def center_on_green(t, motors_down_stage_one, motors_left_stage_one, motors_up_s
         eye_tilt_vel.send_message(std_msgs.msg.Float64(tilt))
         eye_pan_vel.send_message(std_msgs.msg.Float64(pan))
 
-    elif eye_tilt_sub.value is not None and eye_pan_sub.value is not None:
+    else:
         clientLogger.info("Centering on red...")
         # Stage two: Position-controlled motion to red cup
-        delta_theta = 0.02 # Radians per control period
-        current_tilt = eye_tilt_sub.value.data
-        current_pan = eye_pan_sub.value.data
-        tilt = current_tilt + delta_theta * (motors_up_stage_two.voltage - motors_down_stage_two.voltage)
-        pan = current_pan + delta_theta * ( motors_left_stage_two.voltage - motors_right_stage_two.voltage)
-        if tilt - current_tilt #TODO
+        scaling_factor = 0.02
+        joint_names = joint_state_sub.value.name
+        joint_positions = joint_state_sub.value.position
+        current_tilt = joint_positions[joint_names.index("eye_tilt")]
+        current_pan = joint_positions[joint_names.index("left_eye_pan")]
+
+        tilt = current_tilt + scaling_factor * (motors_up_stage_two.voltage - motors_down_stage_two.voltage)
+        pan = current_pan + scaling_factor * ( motors_left_stage_two.voltage - motors_right_stage_two.voltage)
+
         eye_tilt_pos.send_message(std_msgs.msg.Float64(tilt))
         eye_pan_pos.send_message(std_msgs.msg.Float64(pan))
